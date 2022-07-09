5d019e48c36b9034086c998953cda71ec4610404
MattScop <matteo.scopece@outlook.com>
2022-07-09 12:05:29 +0200

    Add styling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // ROCK PAPER SCISSORS GAME!!!
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~OLD COMMENT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /* STEP 1:
 Create a function computerPlay that randomly returns rock, paper, scissors.
 Create an array with those values that the function can take to randomly return one of them.
@@ -28,82 +29,97 @@ Report the winner
 // result. For each round we need to store 1 or 0 depending on who won
 // the match in an array and then the winner is the one whose array has
 //  a length of at least 3.
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~OLD COMMENT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-const arr = ['Rock', 'Paper', 'Scissors'];
+
+
+
+
+
+const RPS = ['Rock', 'Paper', 'Scissors'];
 
 function computerPlay() {
-    let random = Math.floor(Math.random() * arr.length);
-    return arr[random];
-}
+    const randomPlay = Math.floor(Math.random() * RPS.length);
+    const aiCard = document.querySelector('.ai-card');
 
-function playerPlay() {
-    return prompt("Insert Rock, Paper or Scissors");
-}
-
-const computerSelection = computerPlay();
-const computerSelIns = computerSelection.toLowerCase();
-
-const arrComputer = [];
-const arrPlayer = []; 
-
-function declareWinner(arrComputer, arrPlayer) {
-    if (arrComputer.length > arrPlayer.length) {
-        console.log("The Winner is The Computer");
-    } else if (arrComputer.length < arrPlayer.length) {
-        console.log("You Won!");
+    // match picture with computer selection
+    if (RPS[randomPlay] === 'Paper') {
+        aiCard.style.content = 'url(img/paper.png)';
+    } else if (RPS[randomPlay] === 'Rock') {
+        aiCard.style.content = 'url(img/rock.png)';
     } else {
-        console.log("No Winner here, Rematch!");
+        aiCard.style.content = 'url(img/scissors.png)';
     }
-} 
+}
 
-function game() {
-    let loopNum = 5;
-    for (let i = 1; i <= loopNum; i++) {
-        computerPlay();
-        let playerSelection = playerPlay();
-        let playerSeleIns = playerSelection.toLowerCase();
-        const playResult = playRound(computerSelIns, playerSeleIns);
+// function playerPlay() {
+//     return prompt("Insert Rock, Paper or Scissors");
+// }
+
+// const computerSelection = computerPlay();
+// const computerSelIns = computerSelection.toLowerCase();
+
+// const arrComputer = [];
+// const arrPlayer = []; 
+
+// function declareWinner(arrComputer, arrPlayer) {
+//     if (arrComputer.length > arrPlayer.length) {
+//         console.log("The Winner is The Computer");
+//     } else if (arrComputer.length < arrPlayer.length) {
+//         console.log("You Won!");
+//     } else {
+//         console.log("No Winner here, Rematch!");
+//     }
+// } 
+
+// function game() {
+//     let loopNum = 5;
+//     for (let i = 1; i <= loopNum; i++) {
+//         computerPlay();
+//         let playerSelection = playerPlay();
+//         let playerSeleIns = playerSelection.toLowerCase();
+//         const playResult = playRound(computerSelIns, playerSeleIns);
           
 
-        function playRound(computerSelIns, playerSeleIns) {
-            switch (true) {
-                case computerSelIns === playerSeleIns:
-                    return "It's a Tie!";
+//         function playRound(computerSelIns, playerSeleIns) {
+//             switch (true) {
+//                 case computerSelIns === playerSeleIns:
+//                     return "It's a Tie!";
         
-                case computerSelIns === 'rock' && playerSeleIns === 'scissors':
-                    return "You lost this match, Rock beats Scissors!";
-                case computerSelIns === 'rock' && playerSeleIns === 'paper':
-                    return "Congratulations, you won this match! Paper beats Rock.";
+//                 case computerSelIns === 'rock' && playerSeleIns === 'scissors':
+//                     return "You lost this match, Rock beats Scissors!";
+//                 case computerSelIns === 'rock' && playerSeleIns === 'paper':
+//                     return "Congratulations, you won this match! Paper beats Rock.";
         
-                case computerSelIns === 'paper' && playerSeleIns === 'rock':
-                    return "You lost this match, Paper beats Rock!";
-                case computerSelIns === 'paper' && playerSeleIns === 'scissors':
-                    return "Congratulations, you won this match! Scissors beats Paper.";
+//                 case computerSelIns === 'paper' && playerSeleIns === 'rock':
+//                     return "You lost this match, Paper beats Rock!";
+//                 case computerSelIns === 'paper' && playerSeleIns === 'scissors':
+//                     return "Congratulations, you won this match! Scissors beats Paper.";
         
-                case computerSelIns === 'scissors' && playerSeleIns === 'rock':
-                    return "Congratulations, you won this match! Rock beats Scissors.";
-                case computerSelIns === 'scissors' && playerSeleIns === 'paper':
-                    return "You lost this match, Scissors beats Paper!";
+//                 case computerSelIns === 'scissors' && playerSeleIns === 'rock':
+//                     return "Congratulations, you won this match! Rock beats Scissors.";
+//                 case computerSelIns === 'scissors' && playerSeleIns === 'paper':
+//                     return "You lost this match, Scissors beats Paper!";
         
-                default:
-                    return "Something went terribly wrong!";
-            }
-        }
-        console.log(`Match:${i}: ${playerSeleIns}`);
-        console.log(`Match:${i}: ${computerSelIns}`);
-        console.log(playRound(computerSelIns, playerSeleIns));
-        if (playResult.includes('Congratulations')) {
-            arrPlayer.push(i)
-        } else if (playResult.includes('You lost')) {
-            arrComputer.push(i)
-        } else if (playResult.includes("It's a Tie")) {
-            arrPlayer.push(i);
-            arrComputer.push(i);
-        } else {
-            loopNum += 1;
-        }
-    }
-    declareWinner(arrComputer, arrPlayer);
-}
+//                 default:
+//                     return "Something went terribly wrong!";
+//             }
+//         }
+//         console.log(`Match:${i}: ${playerSeleIns}`);
+//         console.log(`Match:${i}: ${computerSelIns}`);
+//         console.log(playRound(computerSelIns, playerSeleIns));
+//         if (playResult.includes('Congratulations')) {
+//             arrPlayer.push(i)
+//         } else if (playResult.includes('You lost')) {
+//             arrComputer.push(i)
+//         } else if (playResult.includes("It's a Tie")) {
+//             arrPlayer.push(i);
+//             arrComputer.push(i);
+//         } else {
+//             loopNum += 1;
+//         }
+//     }
+//     declareWinner(arrComputer, arrPlayer);
+// }
     
-game();
+// game();
